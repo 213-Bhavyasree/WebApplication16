9cf87f16e5f4e54b623477c939561322eaa956db
lokes <lokes@LAPTOP-J2B9HTQV>
2022-01-12 16:07:58 +0530

    Add project files.

---
+++ WebApplication16/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication16.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string DeptName { get; set; }
+        public string Location { get; set; }
+        public int NumberOfStaff { get; set; }
+    }
+}
